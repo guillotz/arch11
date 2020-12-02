b1c4444f10563d433fa933290adeab13a76db71c
Gui <guillermo.fink@reserv.com.ar>
2020-12-02 12:17:26 -0300

    componentes basicos y routeo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Error403Component } from './root/pages/error/error403/error403.component';
+import { Error404Component } from './root/pages/error/error404/error404.component';
+import { LoginComponent } from './root/pages/login/login.component';
+import { RegisterComponent } from './root/pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'error404',
+    component: Error404Component
+  },
+  {
+    path: 'error403',
+    component: Error403Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './root/pages/login/login.component';
+import { RegisterComponent } from './root/pages/register/register.component';
+import { Error404Component } from './root/pages/error/error404/error404.component';
+import { Error403Component } from './root/pages/error/error403/error403.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    Error404Component,
+    Error403Component
   ],
   imports: [
     BrowserModule,
