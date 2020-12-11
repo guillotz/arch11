c29e655172ece27014e1b803d34a454db8cf8695
guillotz <guillermo_fink@hotmail.com>
2020-12-11 13:05:48 -0300

    spinner + servicio

--- src/app/root/components/spinner/spinner.component.ts
+++ src/app/root/components/spinner/spinner.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, AfterViewChecked } from '@angular/core';
+import { Subject } from 'rxjs';
+import { LoaderService } from 'src/app/core/services/loader.service';
+
+
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent implements OnInit, AfterViewChecked {
 
-  constructor() { }
+  isLoading: Subject<boolean> = this.loaderService.isLoading;
+  mostrar? : boolean = false;
+  constructor(private loaderService: LoaderService, private cdRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this.loaderService.isLoading.subscribe(data => {
+      this.mostrar = data;
+    });
+  }
+
+  ngAfterViewChecked(){
+    this.cdRef.detectChanges();
   }
 
 }
--- src/app/root/pages/initial/initial.component.ts
+++ src/app/root/pages/initial/initial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AppService } from 'src/app/core/services/app.service';
 
 @Component({
@@ -10,12 +11,22 @@ export class InitialComponent implements OnInit {
 
   autos? : any[];
   headers?: any;
+  isLoading: boolean = false;
 
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService, private router: Router) { }
 
   ngOnInit(): void {
     this.autos = this.appService.getAutos();
     this.headers = this.appService.getHeadersAutos();
+  }
+
+  go(ruta: string){
+    this.router.navigate([ruta]);
+  }
+
+  showLoading(){
+    //Petición armada para testear el spinner
+    this.appService.getTest().subscribe(data => {alert(data)});
   }
   
 
